e8ba8e6f68f6fbf474bc11394f0c85eadcd14e92
Vitor Barbeiro <vitor.agb@hotmail.com>
2023-01-09 10:11:35 -0300

    User entity and resource

--- src/main/java/com/springboot/course/CourseApplication.java
+++ src/main/java/com/springboot/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.educandoweb.course;
+package com.springboot.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
