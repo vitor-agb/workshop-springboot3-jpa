73cc50713ded480447338e6b6fea60f99fbc5f32
Vitor Barbeiro <vitor.agb@hotmail.com>
2023-01-13 16:19:20 -0300

    Entity Category

---
+++ src/main/java/com/springboot/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.springboot.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.course.entities.Category;
+import com.springboot.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
