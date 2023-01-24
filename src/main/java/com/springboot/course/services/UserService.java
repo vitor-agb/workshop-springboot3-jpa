907ce344614c2380af1dca0dcb34b4a857847446
Vitor Barbeiro <vitor.agb@hotmail.com>
2023-01-24 11:52:08 -0300

    Exception handling - update

--- src/main/java/com/springboot/course/services/UserService.java
+++ src/main/java/com/springboot/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.springboot.course.repositories.UserRepository;
 import com.springboot.course.services.exceptions.DatabaseException;
 import com.springboot.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
